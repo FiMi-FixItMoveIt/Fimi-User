658bdce541b898bb87a7d632222704cfdec763fc
Oyeniyi Michael <47017728+Michaelbarry100@users.noreply.github.com>
2024-02-23 11:06:27 +0100

    Add files via upload

---
+++ lib/utils/configs.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+const APP_NAME = 'FIMI';
+const APP_NAME_TAG_LINE = 'FIX IT MOVE IT Home Services App';
+const DEFAULT_LANGUAGE = 'en';
+
+// You can change this to your provider package name
+const PROVIDER_PACKAGE_NAME = 'com.iqonic.provider';
+
+var defaultPrimaryColor = Color(0xFF5F60B9);
+
+const DOMAIN_URL = '';
+const BASE_URL = '$DOMAIN_URL/api/';
+
+const ONESIGNAL_APP_ID = '01e97a22-4721-475e-a96d-62948ebfbaf4';
+const ONESIGNAL_REST_KEY = "NzFhNDZjYTEtOWUzYS00NzgxLThlZDktODYyYWZmOTQ1ODJk";
+const ONESIGNAL_CHANNEL_ID = "0ee01f0d-2e1c-4554-9050-27dd9c020292";
+
+const TERMS_CONDITION_URL = 'https://iqonic.design/terms-of-use/';
+const PRIVACY_POLICY_URL = 'https://iqonic.design/privacy-policy/';
+const HELP_SUPPORT_URL = 'https://iqonic.desky.support/';
+const PURCHASE_URL =
+    'https://codecanyon.net/item/handyman-service-flutter-ondemand-home-services-app-with-complete-solution/33776097?s_rank=16';
+
+const STRIPE_PAYMENT_PUBLISH_KEY =
+    'pk_test_51GrhA2Bz1ljKAgF98fI6WfB2YUn4CewOB0DNQC1pSeXspUc1LlUYs3ou19oPF0ATcqa52FXTYmv6v0mkvPZb9BSD00SUpBj9tI';
+const STRIPE_MERCHANT_COUNTRY_CODE = 'IN';
+const STRIPE_CURRENCY_CODE = 'INR';
